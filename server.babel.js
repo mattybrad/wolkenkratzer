889f1b5e25751d56d99d53f764f3b606debe2ce4
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2016-10-22 20:31:30 +0100

    server now serves all html files

--- server.babel.js
+++ server.babel.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import path from 'path';
+mongoose.connect('mongodb://localhost/mysite');
+import routes from './server/routes';
 const app = express();
+app.use('/', routes);
 app.use('/', express.static('public'));
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 app.listen(process.env.PORT || 3000);
